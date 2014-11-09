b8a94e6219771989014c4c4f9aeab8bcbcb6453a
Magleff <mail.henry.bonjour@gmail.com>
2014-11-09 23:59:08 +0100

    Center x coordinates at zero, and use translation to center the robot in the viewport.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,42 +21,53 @@ function Drawer(options){
 
         processing.draw = function () {
             processing.scale(scale);
+            //
+            processing.pushMatrix();
+            // Translate
+            processing.translate(150, 0);
+            //
             neck.display();
-            torso.display();
             leftArm.display();
             rightArm.display();
+            torso.display();
             head.display();
             goggles.display();
+            //
+            processing.popMatrix();
         };
 
         function Neck() {
             this.display = function () {
                 processing.rectMode(processing.CENTER);
-                processing.rect(150, 90, 15, 20);
+                processing.rect(0, 90, 15, 20);
             }
         }
 
         function Torso() {
             this.display = function () {
                 processing.beginShape();
-                processing.vertex(70, 110);
-                processing.bezierVertex(80, 100, 220, 100, 230, 110);
-                processing.bezierVertex(250, 110, 260, 120, 220, 250);
-                processing.vertex(80, 250);
-                processing.bezierVertex(40, 120, 50, 110, 70, 110);
+                processing.vertex(-80, 110);
+                processing.bezierVertex(-70, 100, 70, 100, 80, 110);
+                processing.bezierVertex(80, 110, 100, 120, 70, 250);
+                processing.vertex(-70, 250);
+                processing.bezierVertex(-100, 120, -80, 110, -80, 110);
                 processing.endShape();
             }
         }
 
         function LeftArm() {
             this.display = function () {
-                processing.ellipse(40, 130, 40, 40);
+                processing.rectMode(processing.CENTER);
+                processing.rect(-100, 125, 40, 20);
+                processing.ellipse(-110, 125, 40, 40);
             };
         }
 
         function RightArm() {
             this.display = function () {
-                processing.ellipse(260, 130, 40, 40);
+                processing.rectMode(processing.CENTER);
+                processing.rect(100, 125, 40, 20);
+                processing.ellipse(110, 125, 40, 40);
             };
         }
 
@@ -65,10 +76,11 @@ function Drawer(options){
 
                 processing.beginShape();
 
-                processing.arc(150, 60, 60, 80, -processing.PI, 0);
-                processing.line(120, 60, 120, 80);
-                processing.line(120, 80, 180, 80);
-                processing.line(180, 80, 180, 60);
+                // TODO replace this by vertex
+                processing.arc(0, 60, 60, 80, -processing.PI, 0);
+                processing.line(-30, 60, -30, 80);
+                processing.line(-30, 80, 30, 80);
+                processing.line(30, 80, 30, 60);
 
                 processing.endShape();
 
@@ -80,13 +92,13 @@ function Drawer(options){
             this.display = function () {
                 // Shape
                 processing.rectMode(processing.CENTER);
-                processing.rect(150, 60, 70, 30);
+                processing.rect(0, 60, 70, 30);
                 // Eyes
-                processing.ellipse(135, 65, 10, 10);
-                processing.ellipse(165, 60, 15, 15);
+                processing.ellipse(-15, 65, 10, 10);
+                processing.ellipse(15, 60, 15, 15);
                 // Nostrils
-                processing.ellipse(150, 60, 2, 2);
-                processing.ellipse(150, 70, 2, 2);
+                processing.ellipse(0, 60, 2, 2);
+                processing.ellipse(0, 70, 2, 2);
             };
         }
 

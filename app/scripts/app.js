a6a8fa2f09a2ce7add3a97f27cd9c69c01eb553c
Magleff <mail.henry.bonjour@gmail.com>
2014-11-10 00:40:01 +0100

    Add a music box cylinder.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,22 +1,23 @@
-
-function Drawer(options){
+function Drawer(options) {
 
     var scale = options.scale ? options.scale : 1.0;
 
     function draw(processing) {
 
-        var neck, torso, leftArm, rightArm, head, goggles;
+        var neck, torso, cylinder, leftArm, rightArm, head, goggles, teeth;
 
         processing.setup = function () {
             // Viewport
             processing.size(300 * scale, 200 * scale);
             // Robot body parts
             neck = new Neck();
-            torso = new Torso();
             leftArm = new LeftArm();
             rightArm = new RightArm();
+            torso = new Torso();
+            cylinder = new Cylinder();
             head = new Head();
             goggles = new Goggles();
+            teeth = new Teeth();
         };
 
         processing.draw = function () {
@@ -30,7 +31,9 @@ function Drawer(options){
             leftArm.display();
             rightArm.display();
             torso.display();
+            cylinder.display();
             head.display();
+            teeth.display();
             goggles.display();
             //
             processing.popMatrix();
@@ -51,6 +54,22 @@ function Drawer(options){
                 processing.bezierVertex(80, 110, 100, 120, 70, 250);
                 processing.vertex(-70, 250);
                 processing.bezierVertex(-100, 120, -80, 110, -80, 110);
+                processing.endShape();
+            }
+        }
+
+        function Cylinder() {
+            this.display = function () {
+                // Tube
+                processing.rectMode(processing.CENTER);
+                processing.rect(0, 155, 150, 20);
+                // Cylinder
+                processing.beginShape();
+                processing.vertex(-50, 130);
+                processing.bezierVertex(-60, 110, -60, 200, -50, 180);
+                processing.vertex(50, 180);
+                processing.bezierVertex(60, 200, 60, 110, 50, 130);
+                processing.vertex(-50, 130);
                 processing.endShape();
             }
         }
@@ -100,6 +119,23 @@ function Drawer(options){
                 processing.ellipse(0, 60, 2, 2);
                 processing.ellipse(0, 70, 2, 2);
             };
+        }
+
+        function Teeth() {
+            this.display = function () {
+                for (var i = -28; i < 28; i += 5) {
+                    drawTeeth(i);
+                }
+                // Reset translation
+                processing.translate(0, 0);
+            };
+
+            function drawTeeth(x) {
+                processing.pushMatrix();
+                processing.translate(x, 0);
+                processing.rect(0, 100, 3, 53);
+                processing.popMatrix();
+            }
         }
 
     }

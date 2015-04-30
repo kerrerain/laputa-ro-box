100cd8ab78b97e0c213b59cf54ec663091f134a8
h.bonjour <h.bonjour@groupeonepoint.com>
2015-04-30 13:47:35 +0200

    Use vertices to draw the tooth.
    - vertex + bezierVertex.
    - twist the tooth with the mouse.

--- app/scripts/test/test.js
+++ app/scripts/test/test.js
@@ -2,18 +2,6 @@ module.exports = function() {
   'use strict';
   function Test() {}
 
-  function twistEasing(twist, easing) {
-    var result = twist;
-    if (Math.abs(result) > 0) {
-      result += result < 0 ? easing : -easing;
-      result = - result;
-      if (Math.abs(result) < easing) {
-        result = 0;
-      }
-    }
-    return result;
-  }
-
   Test.prototype.draw = function(processing) {
 
     var width = 6;
@@ -21,7 +9,7 @@ module.exports = function() {
     var twistZ = 0;
     var originMouseX = 0;
     var originMouseY = 0;
-    var easing = 5;
+    var absTwistZ = 0;
 
     processing.setup = function () {
       processing.size(400, 400);
@@ -37,17 +25,17 @@ module.exports = function() {
       // Center the shape
       processing.translate(-offset, 0);
 
+      absTwistZ = Math.abs(twistZ);
+
       processing.beginShape();
       processing.vertex(0, 0);
-      processing.bezierVertex(0, 50, 0, 50, 0 - twistZ, 50 + twistZ);
-      processing.vertex(width + twistZ, 50 + twistZ);
-      processing.bezierVertex(width - twistZ, 50, width, 0, width, 0);
+      processing.bezierVertex(0, 50 + absTwistZ, 0 - twistZ, 50 + absTwistZ, 0 - twistZ, 50 + absTwistZ);
+      processing.vertex(width - twistZ, 50 + absTwistZ);
+      processing.bezierVertex(width - twistZ, 50 + absTwistZ, width, 50 + absTwistZ, width, 0);
       processing.vertex(0, 0);
       processing.endShape();
 
       processing.popMatrix();
-
-      twistZ = twistEasing(twistZ, easing);
     };
 
     processing.mousePressed = function() {
@@ -58,6 +46,7 @@ module.exports = function() {
     processing.mouseReleased = function() {
       originMouseX = 0;
       originMouseY = 0;
+      twistZ = 0;
     };
 
     processing.mouseDragged = function() {
